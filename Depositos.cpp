4a60b3239c69c0e95ddc774b20ecdb17a700d9d6
kimberlykamala <kimberly_antequera@hotmail.com>
2016-04-04 23:15:26 -0400

    Create Depositos.cpp

---
+++ Depositos.cpp
@@ -0,0 +1,51 @@
+Depositos.cpp
+
+#include "StdAfx.h"
+#include "Depositos.h"
+
+
+Depositos::Depositos(void)
+{
+}
+
+
+Depositos::~Depositos(void)
+{
+}
+
+int Depositos::cuenta()
+{
+}
+string Depositos::nombre()
+{
+}
+string Depositos::nombreBanco()
+{
+}
+int Depositos::cantidad()
+{
+}
+string Depositos::nombreMercado()
+{
+}
+string Depositos::firma()
+{
+}
+string Depositos::firmaSupervisor()
+{
+}
+string Depositos::concepto()
+{
+}
+Fechas Depositos::F2()
+{
+}
+void Depositos::Comprobante()
+{
+}
+void Depositos::Deposito(Facturas f)
+{
+}
+void Depositos::Pago(Facturas f)
+{
+}
