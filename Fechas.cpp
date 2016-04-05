0b9cd41cbb566efff35feb44daba75bb81d1a757
kimberlykamala <kimberly_antequera@hotmail.com>
2016-04-04 23:14:44 -0400

    Create Fechas.cpp

---
+++ Fechas.cpp
@@ -0,0 +1,29 @@
+#include "StdAfx.h"
+#include "Fechas.h"
+
+
+Fechas::Fechas(void)
+{
+}
+
+Fechas::~Fechas(void)
+{
+}
+int Fechas::hora()
+{
+}
+int Fechas::minuto()
+{
+}
+int Fechas::segundo()
+{
+}
+void Fechas::IngresarHora()
+{
+}
+void Fechas::IngresarMinuto()
+{
+}
+void Fechas::IngresarSegundo()
+{
+}
