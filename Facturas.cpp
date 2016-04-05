9e7105fe563530caca67bef0856ebc7136877b56
kimberlykamala <kimberly_antequera@hotmail.com>
2016-04-04 23:13:51 -0400

    Create Facturas.cpp

---
+++ Facturas.cpp
@@ -0,0 +1,41 @@
+Facturas.cpp
+
+#include "StdAfx.h"
+#include "Facturas.h"
+
+
+Facturas::Facturas(void)
+{
+}
+
+Facturas::~Facturas(void)
+{
+}
+string Facturas::nombre()
+{
+}
+Fechas Facturas:: F1()
+{
+}
+string Facturas::numFactura()
+{
+}
+string Facturas::concepto()
+{
+}
+
+int Facturas::cantidad()
+{
+}
+string Facturas::firma()
+{
+}
+int Facturas::nit()
+{
+}
+void Facturas::LlenarFacturas()
+{
+}
+void Facturas::ActualizarFacturas()
+{
+}
